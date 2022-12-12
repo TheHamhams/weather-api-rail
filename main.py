503edaa69dcb2633a5613805b97b6d83506a3b51
TheHamhams <hamhams86@gmail.com>
2022-12-12 11:49:44 -0800

    removed app

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
+from config import Config
+from routes import api
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models import db as root_db, login_manager, ma
+from flask_cors import CORS
+from helpers import JSONEncoder
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+app.register_blueprint(api)
 
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+app.json_encoder = JSONEncoder
+app.config.from_object(Config)
+root_db.init_app(app)
+login_manager.init_app(app)
+ma.init_app(app)
+migrate = Migrate(app, root_db)
